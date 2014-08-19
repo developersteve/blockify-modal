d42e811cdb676fbbfa65e75c5d87eb987c1b41b7
PP Laptop <steven@developersteve.com>
2014-08-19 21:33:28 +1000

    cleaned up <?php tags added footer button

--- blockify-modal.php
+++ blockify-modal.php
@@ -1,17 +1,26 @@
-<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#<? echo $block->document['name']; ?>">
-  <? echo $block->document['btn-name']; ?>
+<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#<?php echo $block->document['name']; ?>">
+  <?php echo $block->document['btn-name']; ?>
 </button>
 
-<div class="modal fade" id="<? echo $block->document['name']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="<?php echo $block->document['name']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">  <? echo $block->document['modal-title']; ?></h4>
+        <h4 class="modal-title" id="myModalLabel"><?php echo $block->document['modal-title']; ?></h4>
       </div>
       <div class="modal-body">
-		<? $block->document->tag('p', 'body'); ?>
+		<?php $block->document->tag('p', 'body'); ?>
       </div>
+    <?php
+    if($block->document['footer-btn']){
+      ?>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger"><?php echo $block->document['footer-btn'];?></button>
+        </div>
+      <?php
+    }
+      ?>
     </div>
   </div>
 </div>
